832f808fe197faaf9886f2a7a905a7ff7d869aac
Hafsah <inuwahafsah@gmail.com>
2025-11-02 22:45:59 +0100

    K

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
 
+# Add your project to the path
+sys.path.append('/app')
+
+# Set Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visa.settings')
+
+# Import and create Django application
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
